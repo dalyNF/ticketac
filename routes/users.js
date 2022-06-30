7ddd9d5cca05d3a872c3dfb2d16d1a0ee40b8a62
dalyNF <daly.guessan.fouche@gmail.com>
2022-06-30 15:01:05 +0200

    login route

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
-  res.send('login');
+router.get('/', function(req, res, next) {
+  res.render('login');
 });
 
 module.exports = router;
